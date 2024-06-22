77f929fe44123c01f1e959bc4dd4ad3dae8dae2f
Ashvin <ashvinjangid10@gmail.com>
2024-06-22 14:49:36 +0530

    show the books in the ui

--- script.js
+++ script.js
@@ -1,12 +1,49 @@
-const myLibrary = [];
+const books = [];
 
-function Book(title, author, pages, read) {
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.read = read;
+class Book {
+  constructor(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+  }
+
+  // info() {
+  //   return `${this.title}, ${this.author}, ${this.pages}, ${this.read}`
+  // }
 }
 
+function addBookToLibrary(e) {
+  e.preventDefault();
+
+  const title = document.getElementById('title').value;
+  const author = document.getElementById('author').value;
+  const pages = document.getElementById('pages').value;
+  const read = document.getElementById('read').checked;
+
+  const newBook = new Book(title, author, pages, read);
+  // const newBook = new Book("The Hobbit", "J.R.R. Tolkien", 295, false);
+  books.push(newBook);
+  showBooks();
+
+  addBookForm.reset();
+  addBookDialog.close();
+}
+
+function showBooks() {
+  const library = document.querySelector('main');
+  const bookCard = document.createElement('div');
+  books.forEach(book => {
+    bookCard.innerHTML = `
+    <span>${book.title}</span>
+    <div>
+      <button>Not Complete</button>
+      <button>Remove</button>
+    </div>
+    `;
+    library.appendChild(bookCard);
+  });
+}
 
 // Dialog
 
@@ -20,15 +57,4 @@ addBtn.addEventListener('click', () => {
 // Form
 
 const addBookForm = document.querySelector('form');
-
-addBookForm.addEventListener('submit', (e) => {
-  e.preventDefault();
-
-  const title = document.getElementById('title').value;
-  const author = document.getElementById('author').value;
-  const pages = document.getElementById('pages').value;
-  const read = document.getElementById('read').checked;
-
-  const book = new Book(title, author, pages, read);
-  myLibrary.push(book);
-});
+addBookForm.addEventListener('submit', addBookToLibrary);
